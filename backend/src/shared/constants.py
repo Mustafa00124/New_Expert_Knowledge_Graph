de2ed727d4cb3f7f4636ba8ff1cb5387d5e8b020
Mustafa <23100124@lums.edu.pk>
2025-08-17 22:55:59 +0500

    Caps modified

--- backend/src/shared/constants.py
+++ backend/src/shared/constants.py
@@ -7,7 +7,7 @@ GROQ_MODELS = ["groq-llama3"]
 BUCKET_UPLOAD = 'llm-graph-builder-upload'
 BUCKET_FAILED_FILE = 'llm-graph-builder-failed'
 PROJECT_ID = 'llm-experiments-387609' 
-GRAPH_CHUNK_LIMIT = 50 
+GRAPH_CHUNK_LIMIT = 200
 
 
 #query 
@@ -308,7 +308,7 @@ Note: This system does not generate answers based solely on internal knowledge. 
 QUESTION_TRANSFORM_TEMPLATE = "Given the below conversation, generate a search query to look up in order to get information relevant to the conversation. Only respond with the query, nothing else." 
 
 ## CHAT QUERIES
-VECTOR_SEARCH_TOP_K = 5
+VECTOR_SEARCH_TOP_K = 30
 
 VECTOR_SEARCH_QUERY = """
 WITH node AS chunk, score
@@ -335,7 +335,7 @@ RETURN text,
 """ 
 
 ### Vector graph search 
-VECTOR_GRAPH_SEARCH_ENTITY_LIMIT = 40
+VECTOR_GRAPH_SEARCH_ENTITY_LIMIT = 1000
 VECTOR_GRAPH_SEARCH_EMBEDDING_MIN_MATCH = 0.3
 VECTOR_GRAPH_SEARCH_EMBEDDING_MAX_MATCH = 0.9
 VECTOR_GRAPH_SEARCH_ENTITY_LIMIT_MINMAX_CASE = 20
